a615201f238f0ffb8856101fca97c21f38535915
Jiseoup <jseoup@gmail.com>
2025-06-24 02:56:45 +0900

    Initialize database DDL

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
+    join_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE urls (
+    id BIGSERIAL PRIMARY KEY,
+    user_id INTEGER,
+    original_url TEXT NOT NULL,
+    shorten_url VARCHAR(10) NOT NULL UNIQUE,
+    click_count INTEGER NOT NULL DEFAULT 0,
+    created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    expire_date TIMESTAMP WITH TIME ZONE,
+    deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT "fk_urls.user_id" FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE logs (
+    id BIGSERIAL PRIMARY KEY,
+    url_id BIGINT NOT NULL,
+    click_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    ip_address VARCHAR(45) NOT NULL,
+    client_info TEXT NOT NULL,
+    CONSTRAINT "fk_logs.url_id" FOREIGN KEY (url_id) REFERENCES urls(id) ON DELETE CASCADE
+);
